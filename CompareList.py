24c4feeb6b605e21058d541755c77e2cc4ffd4bc
yousramess <45831626+yousramess@users.noreply.github.com>
2026-04-20 11:07:18 -0400

    Add logo 2

--- CompareList.py
+++ CompareList.py
@@ -82,7 +82,14 @@ def dataframe_to_excel_bytes(df):
 
 
 # ================== UI ==================
-st.image("logo.png", width=200)
+col1, col2 = st.columns([1,4])
+
+with col1:
+    st.image("logo.png", width=120)
+
+with col2:
+    st.title("Comparaison Excel (Ref.Indiv)")
+    
 st.title("Comparaison de 2 fichiers Excel (Ref.Indiv)")
 
 st.write(
