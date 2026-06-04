ba71f53cf1a90394e6c635c2364e5e64ef5efac1
yousramess <45831626+yousramess@users.noreply.github.com>
2026-06-04 15:17:54 -0400

    Etiquette

--- Agent_AI.py
+++ Agent_AI.py
@@ -151,6 +151,24 @@ def app_conversion():
     with col2:
         if os.path.exists("logo.png"):
             st.image("logo.png", width=150)
+     # 1. Étiquette d'abord
+
+   col1, col2 = st.columns([1.7, 3], vertical_alignment="center")
+
+    with col1:
+       st.markdown(
+        '<div style="font-size:28px; font-weight:600;">🏷️ Ajouter votre étiquette</div>',
+        unsafe_allow_html=True
+    )
+
+    with col2:
+       etiquette_valeur = st.text_input(
+        "",
+          placeholder="Ajouter ici...",
+          label_visibility="collapsed"
+    )
+
+    return etiquette_valeur
 
     st.subheader("PDF → Excel")
 
@@ -195,7 +213,7 @@ def app_conversion():
                     "Téléphone autre": row["Téléphone maison"],
                     "Francisation": "VRAI",
                     "Usager CARI": "VRAI",
-                    "Étiquette": ""
+                    "Étiquette": etiquette_valeur
                 })
 
             df_final = pd.DataFrame(lignes_finales)
